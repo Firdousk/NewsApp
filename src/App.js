84464c88f37d95f484215b34dc930738fdf82979
Firdous Khan <firdous.khan@vidyamandir.com>
2024-06-05 15:06:52 +0530

    keyprocess

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
 import LoadingBar from 'react-top-loading-bar';
 export default class App extends Component {
   state={progress:10}
+
+  apiKey= process.env.REACT_APP_NEWS_API;
 setProgess=(progress)=>{
     this.setState({
       progress:progress
@@ -26,14 +28,14 @@ setProgess=(progress)=>{
        
       />
         <Routes>
-          <Route exact path="/" element={<News setProgess={this.setProgess}  key="general" pageSize={8} country='in' category='general'/>} />
-          <Route exact path="/business" element={<News setProgess={this.setProgess}  key="business" pageSize={8} country='in' category='Business'/> }/>
-          <Route exact path="entertainment"  element={<News setProgess={this.setProgess} key="entertainment" pageSize={8} country='in' category='Entertainmet'/>} />
-          <Route exact path="/general" element={<News setProgess={this.setProgess} key="general" pageSize={8} country='in' category='General'/>} />
-          <Route exact path="/health" element={<News setProgess={this.setProgess} key="health" pageSize={8} country='in' category='Health' />}/>
-          <Route exact path="/science" element={<News setProgess={this.setProgess} key="science" pageSize={8} country='in' category='Science' />}/>
-          <Route exact path="/sports" element={<News setProgess={this.setProgess} key="sports" pageSize={8} country='in' category='Sports' />}/>
-          <Route exact path="/technology" element={<News setProgess={this.setProgess} key="technology" pageSize={8} country='in' category='Technology' />}/>
+          <Route exact path="/" element={<News setProgess={this.setProgess} apiKey ={this.apiKey} key="general" pageSize={8} country='in' category='general'/>} />
+          <Route exact path="/business" element={<News setProgess={this.setProgess} apiKey ={this.apiKey} key="business" pageSize={8} country='in' category='Business'/> }/>
+          <Route exact path="entertainment"  element={<News setProgess={this.setProgess} apiKey ={this.apiKey}key="entertainment" pageSize={8} country='in' category='Entertainmet'/>} />
+          <Route exact path="/general" element={<News setProgess={this.setProgess} apiKey ={this.apiKey}key="general" pageSize={8} country='in' category='General'/>} />
+          <Route exact path="/health" element={<News setProgess={this.setProgess} apiKey ={this.apiKey}key="health" pageSize={8} country='in' category='Health' />}/>
+          <Route exact path="/science" element={<News setProgess={this.setProgess} apiKey ={this.apiKey}key="science" pageSize={8} country='in' category='Science' />}/>
+          <Route exact path="/sports" element={<News setProgess={this.setProgess} apiKey ={this.apiKey}key="sports" pageSize={8} country='in' category='Sports' />}/>
+          <Route exact path="/technology" element={<News setProgess={this.setProgess} apiKey ={this.apiKey}key="technology" pageSize={8} country='in' category='Technology' />}/>
  
         </Routes>
         </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -28,7 +28,7 @@ export class News extends Component {
 
     async updateNews(){
       this.props.setProgess(10);
-     const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ff73904ad68e42f385eed900986dbb30&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+     const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({loading :true});
 
       
@@ -52,7 +52,7 @@ export class News extends Component {
     }
     fetchMoreData = async() => {
       this.setState({page:this.state.page+1})
-      const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ff73904ad68e42f385eed900986dbb30&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       
       let data= await fetch(url);
       let parseData= await data.json()
